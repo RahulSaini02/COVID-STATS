c3cb834fc47f09ffbbea3f17f5aa16bd47dc458d
rahulsaini0219 <60145016+rahulsaini0219@users.noreply.github.com>
2020-09-14 13:39:45 +0530

    World Illustraton

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 import "./Home.css";
-import Circle from "./illustration.png";
+import World from "./World.png";
 function Content() {
   return (
     <div className="Content">
@@ -16,13 +17,14 @@ function Content() {
         <div className="Content__more">
           <button>
             <a href="https://www.who.int/health-topics/coronavirus#tab=tab_1">
-              Learn More
+              <ArrowForwardIcon />
             </a>
           </button>
+          <h2>See More</h2>
         </div>
       </div>
-      <div className="Content_Img">
-        <img src={Circle} alt="" />
+      <div className="Content__Img">
+        <img src={World} alt="" />
       </div>
     </div>
   );
